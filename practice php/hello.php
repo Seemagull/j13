ecd2c5fab197b6f338f46c40f9422bdc72c30d84
Seemagull <56857634+Seemagull@users.noreply.github.com>
2020-01-10 10:59:13 +0500

    Add files via upload

---
+++ practice php/hello.php
@@ -0,0 +1,40 @@
+<?php
+$txt ="<h1> Hello world! </h1>";
+echo "$txt";
+echo "<br>";
+$x=1;
+while($x<=10){
+	echo "name is seema";
+	echo "<br>";
+	$x++;
+}
+
+
+
+
+?>
+<?php
+$x=0;
+while($x<=50)
+{
+	echo "this is number is:$x <br>";
+    $x=$x+10;	
+}
+?>
+
+<html>
+<body>
+
+<?php  
+$x = 0;
+ 
+while($x <= 100) 
+{
+  echo "The number is: $x <br>";
+  $x+=10;
+}
+?>  
+
+</body>
+</html>
+
